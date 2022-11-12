bad6693ddc67d441525d52c426d664970f4a732c
adil-khursheed <adilkhursheed60@gmail.com>
2022-11-12 22:23:21 +0530

    logic added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const defaultMonth = document.getElementById("default-month");
 const defaultYear = document.getElementById("default-year");
 const defaultPin = document.getElementById("default-pin");
 const cardHolderName = document.getElementById("card_holder_name");
+const nameError = document.getElementById("name-error");
 const cardNumber = document.getElementById("card-number");
 const numberError = document.getElementById("number-error");
 const expMonth = document.getElementById("exp_month");
@@ -13,37 +14,193 @@ const cvvPin = document.getElementById("security-pin");
 const pinError = document.getElementById("pin-error");
 const submitBtn = document.getElementById("submit-btn");
 const resetBtn = document.getElementById("reset-btn");
+const cardForm = document.querySelector(".card-form");
+const completeState = document.querySelector(".completed-state");
+
+// Card Name Validation
+cardHolderName.addEventListener('input', () => {
+    
+    defaultName.innerText = cardHolderName.value;
+
+    let regExp = /[0-9]/g;
+    if (regExp.test(cardHolderName.value)) {
+        showError(cardHolderName, nameError, 'Wrong format, alphabets only');
+
+    } else {
+        showError(cardHolderName, nameError, '', false);
+    }
+
+    if (cardHolderName.value == "") {
+        defaultName.innerText = "Jane Appleseed";
+    }
+})
 
 
-const inputName = () => {
-    defaultName.innerHTML = cardHolderName.value;
-    if (defaultName.innerHTML == "") {
-        let name = "Jane Appleseed"
-        defaultName.innerHTML = name;
+// Card Number Validation
+    cardNumber.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^\w]/g, '').replace(/(.{4})/g, '$1 ').trim();
+
+        defaultNumber.innerText = cardNumber.value;
+
+        let regExp = /[A-z]/g;
+        if (regExp.test(cardNumber.value)) {
+            showError(cardNumber, numberError, 'Wrong format, numbers only');
+
+        } else {
+            showError(cardNumber, numberError, '', false);
+        }
+        
+        if (cardNumber.value == "") {
+            defaultNumber.innerText = "0000 0000 0000 0000";
+        }
+    });
+
+
+// Month Validation
+expMonth.addEventListener('input', () => {
+
+    defaultMonth.innerText = expMonth.value;
+
+    validateLetters(expMonth, dateError);
+
+    if (expMonth.value == "") {
+        defaultMonth.innerText = "00";
+    }
+})
+
+
+
+// Year Validation
+expYear.addEventListener('input', () => {
+
+    defaultYear.innerText = expYear.value;
+
+    validateLetters(expYear, dateError);
+
+    if (expYear.value == "") {
+        defaultYear.innerText = "00";
+    }
+})
+
+
+// CVV Validation
+cvvPin.addEventListener('input', () => {
+
+    defaultPin.innerText = cvvPin.value;
+
+    validateLetters(cvvPin, pinError);
+
+    if (cvvPin.value == "") {
+        defaultPin.innerText = "000";
+    }
+})
+
+
+// Form Submission
+let nameValidation = false;
+let numberValidation = false;
+let monthValidation = false;
+let yearValidation = false;
+let cvvValidation = false;
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    // Validate Name
+    if (verifyIsFilled(cardHolderName, nameError)) {
+        nameValidation = true;
+    } else {
+        nameValidation = false;
+    }
+
+    // Validate number
+    if (verifyIsFilled(cardNumber, numberError) == true) {
+        if (cardNumber.value.length == 19) {
+            showError(cardNumber, numberError, '', false);
+            numberValidation = true;
+        } else {
+            showError(cardNumber, numberError, 'Please enter a valid number');
+            numberValidation = false;
+        }
+    }
+
+    // Validate Month 
+    if (verifyIsFilled(expMonth, dateError)) {
+        if (parseInt(expMonth.value) > 0 && parseInt(expMonth.value) <= 12) {
+            showError(expMonth, dateError, '', false);
+            monthValidation = true;
+        } else {
+            showError(expMonth, dateError, 'Please enter a valid month');
+            monthValidation = false;
+        }
+    }
+
+    // Validate Year
+    if (verifyIsFilled(expYear, dateError)) {
+        if (parseInt(expYear.value) > 22 && parseInt(expYear.value) <= 32) {
+            showError(expYear, dateError, '', false);
+            yearValidation = true;
+        } else {
+            showError(expYear, dateError, 'Please enter a valid year');
+            yearValidation = false;
+        }
+    }
+
+    // Validate Cvv
+    if (verifyIsFilled(cvvPin, pinError)) {
+        if (cvvPin.value.length == 3) {
+            showError(cvvPin, pinError, '', false);
+            cvvValidation = true;
+        } else {
+            showError(cvvPin, pinError, 'Please enter a valid CVV');
+            cvvValidation = false;
+        }
+    }
+
+    if (nameValidation == true && numberValidation == true && monthValidation == true && yearValidation == true && cvvValidation == true) {
+        
+        cardForm.style.display = 'none';
+        completeState.style.display = 'block';
+    }
+})
+
+
+// Form Reset
+resetBtn.addEventListener('click', () => {
+    cardForm.style.display = 'block';
+    completeState.style.display = 'none';
+})
+
+
+// Functions
+
+function showError(divInput, divError, msgError, show = true){
+    if(show) {
+        divError.innerText = msgError;
+        divInput.style.setProperty('--validationColor', 'linear-gradient(white, white), linear-gradient(90deg, hsl(0, 100%, 66%), hsl(0, 100%, 66%))');
+        divInput.style.setProperty('--validColor', 'hsl(0, 100%, 66%)');
+    } else {
+        divError.innerText = msgError;
+        divInput.style.setProperty('--validationColor', 'linear-gradient(white, white), linear-gradient(90deg, hsl(249, 99%, 64%), hsl(278, 94%, 30%))');
+        divInput.style.setProperty('--validColor', 'hsl(270, 3%, 87%)');
     }
 }
 
-const inputNumber = () => {
-    cardNumber.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^\w]/g, '').replace(/(.{4})/g, '$1 ').trim();
-    });
-    
-    let cardNumberInput = cardNumber.value;
-    defaultNumber.innerHTML = cardNumberInput;
-    if (defaultNumber.innerHTML == "") {
-        let number = "0000 0000 0000 0000"
-        defaultNumber.innerHTML = number;
-    }
-
-    if (!cardNumberInput.match(/^[0-9]{20}$/)) {
-        cardNumber.style.setProperty('--validationColor', 'linear-gradient(white, white), linear-gradient(90deg, hsl(0, 100%, 66%), hsl(0, 100%, 66%))');
-        numberError.innerHTML = 'Wrong format, numbers only';
-
+function verifyIsFilled(divInput, divError){
+    if(divInput.value.length > 0) {
+        showError(divInput, divError, "", false);
+        return true;
+    } else {
+        showError(divInput, divError, "Can't be blank");
         return false;
     }
+}
 
-    cardNumber.style.setProperty('--validationColor', 'linear-gradient(white, white), linear-gradient(90deg, hsl(249, 99%, 64%), hsl(278, 94%, 30%))');
-    numberError.innerHTML = '';
-
-    return true;
+function validateLetters(input, divError){
+    let regExp = /[A-z]/g;
+    if(regExp.test(input.value)){
+        showError(input, divError, 'Wrong format, numbers only');
+    }else{
+        showError(input, divError, '', false);
+    }
 }
